00abd407983652e64c90e286e00d6299cb17baf0
Ubuntu <ubuntu@ip-172-31-49-169.ec2.internal>
2023-12-09 09:10:40 +0000

    added blue color and h5 to blue-useast1

--- react/src/beforeLogin/Login.js
+++ react/src/beforeLogin/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
         <img src="images/logo.png" alt="" />
         <h1>Quiz Network</h1>
         <h2>Welcome Back</h2>
-        <h2>Blue Deployment</h2>
+        <h5>[Blue Deployment]</h5>
         <p>Please enter your details</p>
         <div className={styles.inputBox}>
           <span>Username</span>
