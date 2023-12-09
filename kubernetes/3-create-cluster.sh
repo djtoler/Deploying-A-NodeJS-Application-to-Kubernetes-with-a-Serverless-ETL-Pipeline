7423bec38d66fd5e7a75c61f129b798f485b05d9
Ubuntu <ubuntu@ip-172-31-49-169.ec2.internal>
2023-12-09 20:26:15 +0000

    updates cluster script

--- kubernetes/3-create-cluster.sh
+++ kubernetes/3-create-cluster.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
-#Inputs for Kubernetes commands
+# Inputs for Kubernetes commands
 #######################################################################################################################
-CLUSTER_NAME=$1
-NODE_GROUP_FE_NAME=$2
-NODE_GROUP_FE_COUNT=$3
-NODE_GROUP_FE_TYPE=$4
-NODE_GROUP_BE_NAME=$5
-NODE_GROUP_BE_COUNT=$6
-NODE_GROUP_BE_TYPE=$7
-PRIVATE_SUBNET1=$8
-PRIVATE_SUBNET2=$9
-PUBLIC_SUBNET1=$10
-PUBLIC_SUBNET2=$11
+CLUSTER_NAME="$1"
+NODE_GROUP_FE_NAME="$2"
+NODE_GROUP_FE_COUNT="$3"
+NODE_GROUP_FE_TYPE="$4"
+NODE_GROUP_BE_NAME="$5"
+NODE_GROUP_BE_COUNT="$6"
+NODE_GROUP_BE_TYPE="$7"
+PRIVATE_SUBNET1="$8"
+PRIVATE_SUBNET2="${9}"
+PUBLIC_SUBNET1="${10}"
+PUBLIC_SUBNET2="${11}"
 
-#Create Kubernetes cluster in at least 2 private & 2 public subnets
-#Create Node Groups for frontend/backend and associate IAM with OIDC for Kubernetes to use AWS services
+# Create Kubernetes cluster in at least 2 private & 2 public subnets
+# Create Node Groups for frontend/backend and associate IAM with OIDC for Kubernetes to use AWS services
 #########################################################################################################################
-eksctl create cluster $CLUSTER_NAME  --vpc-private-subnets=subnet-$PRIVATE_SUBNET1,subnet-$PRIVATE_SUBNET2  --vpc-public-subnets=subnet-$PUBLIC_SUBNET1,subnet-$PUBLIC_SUBNET2 --without-nodegroup
-eksctl create nodegroup --cluster $CLUSTER_NAME --name $NODE_GROUP_FE_NAME --node-type $NODE_GROUP_FE_TYPE --nodes $NODE_GROUP_FE_COUNT
-eksctl create nodegroup --cluster $CLUSTER_NAME --name $NODE_GROUP_BE_NAME --node-type $NODE_GROUP_BE_TYPE --nodes $NODE_GROUP_BE_COUNT--node-private-networking --subnet-ids subnet-$PRIVATE_SUBNET1,subnet-$PRIVATE_SUBNET2 --node-labels "role=backend"
-eksctl utils associate-iam-oidc-provider --cluster $CLUSTER_NAME --approve
+eksctl create cluster "$CLUSTER_NAME" --vpc-private-subnets="$PRIVATE_SUBNET1,$PRIVATE_SUBNET2" --vpc-public-subnets="$PUBLIC_SUBNET1,$PUBLIC_SUBNET2" --without-nodegroup
+eksctl create nodegroup --cluster "$CLUSTER_NAME" --name "$NODE_GROUP_FE_NAME" --node-type "$NODE_GROUP_FE_TYPE" --nodes "$NODE_GROUP_FE_COUNT"
+eksctl create nodegroup --cluster "$CLUSTER_NAME" --name "$NODE_GROUP_BE_NAME" --node-type "$NODE_GROUP_BE_TYPE" --nodes "$NODE_GROUP_BE_COUNT" --node-private-networking --subnet-ids "$PRIVATE_SUBNET1,$PRIVATE_SUBNET2" --node-labels "role=backend"
+eksctl utils associate-iam-oidc-provider --cluster "$CLUSTER_NAME" --approve
 
-#Apply deployment files for making pods & service files for routing to pods
+# Apply deployment files for making pods & service files for routing to pods
 ##########################################################################################################################
-./green-useast2-apply-yamls.sh
+./blue-useast1-apply-yamls.sh
 
-#Download & create IAM policy for EKS Loadbalancer if it doesnt already exist
+# Download & create IAM policy for EKS Loadbalancer if it doesn't already exist
 ##########################################################################################################################
-#wget https://raw.githubusercontent.com/kura-labs-org/Template/main/iam_policy.json
-#aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam_policy.json
+# wget https://raw.githubusercontent.com/kura-labs-org/Template/main/iam_policy.json
+# aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam_policy.json
 
-#Create service account & certificate manager
+# Create service account & certificate manager
 ##########################################################################################################################
-eksctl create iamserviceaccount --cluster=$CLUSTER_NAME --namespace=kube-system --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::994181039877:policy/AWSLoadBalancerControllerIAMPolicy --approve
+eksctl create iamserviceaccount --cluster="$CLUSTER_NAME" --namespace=kube-system --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::994181039877:policy/AWSLoadBalancerControllerIAMPolicy --approve
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.5.4/cert-manager.yaml
 
-#Download configurations for the ingress controller & load balancer controller & give resources time to create
+# Download configurations for the ingress controller & load balancer controller & give resources time to create
 ##########################################################################################################################
 # wget https://raw.githubusercontent.com/kura-labs-org/Template/main/v2_4_7_full.yaml
 sleep 10s
@@ -45,7 +45,7 @@ sleep 10s
 kubectl apply -f v2_4_7_full.yaml 
 sleep 10s
 
-#Apply ingress-class, ingress & give resources time to create
+# Apply ingress-class, ingress & give resources time to create
 ##########################################################################################################################
 kubectl apply -f ingress_class.yaml
 sleep 20s
