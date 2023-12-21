ec3ff610e7e388dfe2279e8e627fb3a0769b590d
djtoler <dwaynetoler@gmail.com>
2023-12-21 09:26:34 +0000

    14file

--- kubernetes/microservices/ms-maker14.sh
+++ kubernetes/microservices/ms-maker14.sh
@@ -1,37 +1,26 @@
 #!/bin/bash
 
-# Store the name of the current branch
-current_branch="green-useast2"
+# Loop through each subdirectory
+for dir in */ ; do
+    # Remove trailing slash to get the directory name
+    dir_name="${dir%/}"
 
-# Fetch the latest state of your branches
-git fetch origin
-
-# Loop through each directory (microservice)
-for dir in microservices/*/ ; do
-    # Get the directory name without the path
-    dir_name="${dir#microservices/}"
-    dir_name="${dir_name%/}"
-
-    # Check out the corresponding branch for the microservice
+    # Checkout the corresponding branch
     git checkout "$dir_name"
 
-    # Remove all content except the microservice directory
-    # This command will keep the directory and delete everything else
-    find . -mindepth 1 -not -regex "^./$dir_name\(/.*\)?$" -delete
+    # Loop through all files in the directory and prepend '1' to each file
+    for file in "$dir_name"/* ; do
+        if [ -f "$file" ]; then  # Check if it's a file
+            echo "1" | cat - "$file" > temp && mv temp "$file"
+        fi
+    done
 
-    # Move the contents of the microservice directory to the root of the branch
-    mv "$dir_name"/* .
-    rmdir "$dir_name"  # Remove the now empty directory
+    # Add all changes within the directory
+    git add "$dir_name/"
 
-    # Add changes to git
-    git add .
+    # Commit the changes
+    git commit -m "Modified files in $dir_name"
 
-    # Commit changes
-    git commit -m "Restructure $dir_name contents"
-
-    # Push changes to the corresponding remote branch
+    # Push to the corresponding remote branch
     git push origin "$dir_name"
-
-    # Switch back to the original branch
-    git checkout "$current_branch"
 done
